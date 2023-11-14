ced190fd04828f61ee7304e813727da8fd19fb00
Pesanth Janaseth Rangaswamy Anitha <pesanth10@gmail.com>
2023-11-13 22:41:19 -0400

    Update Sell Car Component Form State
    change 4 states for each form field to one form state object

--- react_frontend/src/components/SellCarComponent.jsx
+++ react_frontend/src/components/SellCarComponent.jsx
@@ -10,30 +10,23 @@ export default function SellCarComponent(){
 
   //brand, color, type, age
 
-  const [brand, setBrand] = useState("Toyota");
-  const [color, setColor] = useState("#000000");
-  const [type, setType] = useState("SUV");
-  const [age, setAge] = useState();
+  const [form, setForm] = useState({
+    brand: "Toyota",
+    color: "#000000",
+    type: "SUV",
+    age: 0,
+  });
 
-  function handleBrandChange(event){
-    setBrand(event.target.value)
-  }
-
-  function handleColorChange(event){
-    setColor(event.target.value);
-  }
-
-  function handleTypeChange(event){
-    setType(event.target.value)
-  }
-
-  function handleAgeChange(event){
-    setAge(event.target.value);
+  function handleFormChange(event){
+    setForm({
+      ...form,
+      [event.target.name]:event.target.value
+    })
   }
 
   const submit = async (e) => {
     e.preventDefault();
-     await listCarForSale(brand, color, type, age)
+     await listCarForSale(form.brand, form.color, form.type, form.age)
      .then(() => {
       navigate("/my-cars")
      })
@@ -48,7 +41,7 @@ export default function SellCarComponent(){
       <form method="POST" onSubmit={submit}>
         <div className="form-row">
           {/* <label name="brand" className="label-add-car">Brand: </label> */}
-          <select name="brand" value={brand} onChange={handleBrandChange}>
+          <select name="brand" value={form.brand} onChange={handleFormChange}>
             <option value="Toyota">Toyota</option>
             <option value="Audi">Audi</option>
             <option value="Ferrari">Ferrari</option>
@@ -58,12 +51,12 @@ export default function SellCarComponent(){
 
         <div className="form-row">
           {/* <label name="color" className="label-add-car">Color: </label> */}
-          <input name="color" type="color" value={color} className="input-color" onChange={handleColorChange} ></input>
+          <input name="color" type="color" value={form.color} className="input-color" onChange={handleFormChange} ></input>
         </div>
 
         <div className="form-row">
             {/* <label name="type" className="label-add-car">Type: </label> */}
-            <select name="type" value={type} onChange={handleTypeChange} >
+            <select name="type" value={form.type} onChange={handleFormChange} >
                 <option value="SUV">SUV</option>
                 <option value="Sedan">Sedan</option>
                 <option value="Sports Car">Sports Car</option>
@@ -77,7 +70,7 @@ export default function SellCarComponent(){
 
         <div className="form-row">
             {/* <label name="age" className="label-add-car">Age: </label> */}
-            <input name="age" type="number" placeholder="Age of Car in Years" value={age} min={0} onChange={handleAgeChange} className="input-text input-number"></input>
+            <input name="age" type="number" placeholder="Age of Car in Years" value={form.age} min={0} onChange={handleFormChange} className="input-text input-number"></input>
         </div>
 
         <button type="submit" className="btn" >Add Car</button>
